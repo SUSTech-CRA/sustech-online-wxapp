e1ac7e0c3ffc176a89bdc8463f292f34f26f528f
liziwl <leezisy@gmail.com>
2023-12-10 17:15:21 +0800

    update

--- pages/bus/index.js
+++ pages/bus/index.js
@@ -1,18 +1,34 @@
 // pages/bus/index.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    url: app.globalData.DEFAULT_BUS_URL
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (query) {
+    let tmp_url = app.globalData.entry_url;
+    app.globalData.entry_url = null;
+    console.log("获取 app.globalData.entry_url: ", tmp_url);
+    if (tmp_url) {
+      // console.log("onload")
+      console.log("container url: ", tmp_url)
+      this.setData({
+        url: tmp_url
+      })
+    } else {
+      console.log("container url（缺省）: ", app.globalData.DEFAULT_BUS_URL)
+      this.setData({
+        url: app.globalData.DEFAULT_BUS_URL
+      })
+    }
   },
 
   /**
@@ -26,13 +42,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-      if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 1
-        })
-      }
-    },
+    if (typeof this.getTabBar === 'function' &&
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 1
+      })
+    }
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
@@ -65,9 +81,32 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage(options) {
+    let shareTitle = this.data.shareTitle;
+    let shareURL = this.data.shareURL;
+    let args = encodeURIComponent('?utm_source=wx&utm_medium=share_card');
+    if (options.webViewUrl) {
+      var encode_url = "url=" + encodeURIComponent(options.webViewUrl) + args
+    } else {
+      var encode_url = "url=" + encodeURIComponent(shareURL) + args
+    }
+    console.log("分享链接: ", encode_url);
     return {
-      title: "校园巴士时刻表"
+      title: shareTitle,
+      path: "pages/bus/index?" + encode_url,
+    }
+  },
+
+  /**
+   * 绑定心跳函数，获取标题、URL信息
+   */
+  getMSG(e) {
+    let last_item = e.detail.data.pop()
+    if (last_item.msgType == "heartbeat") {
+      this.setData({
+        shareTitle: last_item.shareTitle,
+        shareURL: last_item.shareURL
+      });
     }
   }
 })
