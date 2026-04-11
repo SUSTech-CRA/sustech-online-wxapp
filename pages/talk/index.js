7b52948867752cc8f8e6e9a494ead6fa2e1af517
liziwl <leezisy@gmail.com>
2026-04-11 10:42:10 +0800

    feat: 支持动态分享标题和心跳消息接收

--- pages/talk/index.js
+++ pages/talk/index.js
@@ -41,11 +41,34 @@ Page({
 
   },
 
-  onShareAppMessage() {
+  onShareAppMessage(options) {
+    let shareTitle = this.data.shareTitle || "SUSTech Talks";
+    let shareURL = this.data.shareURL || app.globalData.DEFAULT_TALK_URL;
+    let args = encodeURIComponent('?utm_source=wx&utm_medium=share_card');
+    if (options.webViewUrl) {
+      var encode_url = "url=" + encodeURIComponent(options.webViewUrl) + args
+    } else {
+      var encode_url = "url=" + encodeURIComponent(shareURL) + args
+    }
+    console.log("分享链接: ", encode_url);
     return {
-      title: "SUSTech Talks"
+      title: shareTitle,
+      path: "pages/index/index?" + encode_url,
     }
   },
+
+  /**
+   * 绑定心跳函数，获取标题、URL信息
+   */
+  getMSG(e) {
+    let last_item = e.detail.data.pop()
+    if (last_item.msgType == "heartbeat") {
+      this.setData({
+        shareTitle: last_item.shareTitle,
+        shareURL: last_item.shareURL
+      });
+    }
+  }
 
 })
 
